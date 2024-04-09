c43541a9a3c970e0cdd4b24798b40153dec78e6f
Jash Gusani <143817091+Jashgusani123@users.noreply.github.com>
2024-04-09 15:17:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
+  let size = document.getElementById("fsize");
     let text = document.getElementById("textarea");
     let bgcolor = document.getElementById("incolor");
     let op = document.getElementById("output");
@@ -11,14 +12,21 @@ btn.addEventListener("click", function () {
     div.style.borderTop = "10px solid black";
     div.style.borderRadius = "10px";
     div.style.padding = "10px";
-    div.style.fontSize = " x-large";
+    div.style.fontSize = `${size.value}`;
+  //console.log(`${size.value}px` )
     div.style.fontFamily = "Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
-    div.style.backgroundColor = `${bgcolor.value}`
-    if(bgcolor.value == "#000000"){
-        div.style.color ="white";
-    }
-    div.innerText = `${text.value}`;
-    // console.log(text);
-    op.prepend(div);
+   div.style.backgroundColor = `${bgcolor.value}`
+   if(bgcolor.value == "#000000"){
+      div.style.color ="white";
+     div.style.borderTop = "10px solid red";
+     div.style.borderBottom = "10px solid red";
+   }
+ if(text.value.length <30){
+   div.style.width="300px";
+ }
+  
+   div.innerText = `${text.value}`;
+  //  console.log(text.value.length);
+   op.prepend(div);
     document.getElementById("textarea").value = "";
 })
